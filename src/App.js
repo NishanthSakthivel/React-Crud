752ff5c9f3ed9359c72219fc19dac7a6a4362e92
Nishanth <rsvnishanth95@outlook.com>
2021-06-14 12:37:53 +0530

    first commit with get and post ops

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./App.css";
+import RenderItemsList from './components/renderItemsList'
+import AddItem from "./components/AddItem";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container">
+        <Navbar />
+        <br/>
+        <Route path="/" exact component={RenderItemsList} />
+        <Route path="/additems"  component={AddItem} />
+      </div>
+    </Router>
   );
 }
 
